81411dd7d8cc47e7152e008dd501ca34463373bc
Eva <mt181104@fhstp.ac.at>
2021-01-21 08:22:29 +0100

    Cordova things

--- typings/cordova-typings.d.ts
+++ typings/cordova-typings.d.ts
@@ -1,3 +1,5 @@
 
 /// <reference path="..\.vscode\typings\cordova\cordova.d.ts"/>
 /// <reference path="..\.vscode\typings\cordova\plugins\Toast.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/cordova.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/Toast.d.ts"/>
